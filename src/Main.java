db09e562d80bef4aa55c0e412948926e62f9c29b
whileTrue0x90 <whiletrue0x90@whileTrue0x90deMacBook-Pro.local>
2020-12-16 15:25:53 +0800

    add getNextDay

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello JAVA");
+    }
+
+    public static Date getNextDay() {
+        try {
+            Thread.sleep(24 * 60 * 60 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return new Date();
     }
 }
